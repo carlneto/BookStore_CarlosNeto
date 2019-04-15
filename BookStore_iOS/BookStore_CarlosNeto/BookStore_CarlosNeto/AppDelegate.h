3198e68c457622fdb2b52b59d57c5d4215a3cbdf
Carlos Neto <carlos.neto@pt.softinsa.com>
2019-04-15 16:47:47 +0100

    Project initialisation

---
+++ BookStore_iOS/BookStore_CarlosNeto/BookStore_CarlosNeto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BookStore_CarlosNeto
+//
+//  Created by Carlos Neto on 15/04/2019.
+//  Copyright © 2019 Carlos Neto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
