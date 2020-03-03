19d915c06bba0ce4a126057e5346ebbf7d5445dd
Carlos Neto <carlneto@icloud.com>
2020-03-03 12:55:45 +0000

    removes unused methods

--- BookStore_iOS/BookStore_CarlosNeto/BookStore_CarlosNetoUITests/BookStore_CarlosNetoUITests.swift
+++ BookStore_iOS/BookStore_CarlosNeto/BookStore_CarlosNetoUITests/BookStore_CarlosNetoUITests.swift
@@ -15,10 +15,6 @@ class BookStore_CarlosNetoUITests: XCTestCase {
         continueAfterFailure = false
     }
 
-    override func tearDown() {
-        super.tearDown()
-    }
-
     func testFavSwitchOn_success() {
         let app = XCUIApplication()
         app.launch()
@@ -34,14 +30,5 @@ class BookStore_CarlosNetoUITests: XCTestCase {
         favSwitch.tap()
         favSwitch.swipeLeft()
         XCTAssertFalse((favSwitch.value as? String) == "1")
-    }
-
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
